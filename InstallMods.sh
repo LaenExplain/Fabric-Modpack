27a278f7df526513b3464b2193f959999682a4b8
Laen111 <Laen111@users.noreply.github.com>
2022-06-11 23:47:07 -0400

    Script to quickly add all the mods I like to a new version

---
+++ InstallMods.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+PACKWIZ="../packwiz"
+CURSE="${PACKWIZ} cf install"
+MODRINTH="${PACKWIZ} mr install"
+
+
+$MODRINTH advancementinfo
+$MODRINTH animatica
+$CURSE better-fps-render-distance-fabric
+$MODRINTH better-mount-hud
+$MODRINTH betterf3
+$MODRINTH borderless-mining
+$MODRINTH c2me-fabric
+$MODRINTH capes
+$MODRINTH cem
+$MODRINTH cit-resewn
+$MODRINTH cloth-config
+$MODRINTH continuity
+$MODRINTH cull-less-leaves
+$MODRINTH debugify
+$MODRINTH dynamic-fps
+$MODRINTH ebe
+$MODRINTH entityculling
+$MODRINTH entitytexturefeatures
+$MODRINTH fabric-api
+$MODRINTH fabric-language-kotlin
+$MODRINTH fabrishot
+$CURSE farsight-fabric
+$MODRINTH ferrite-core
+$MODRINTH indium
+$MODRINTH iris
+$MODRINTH krypton
+$MODRINTH ksyxis
+$MODRINTH lambdabettergrass
+$MODRINTH lambdynamiclights
+$MODRINTH lazydfu
+$MODRINTH lithium
+$MODRINTH memoryleakfix
+$MODRINTH modmenu
+$MODRINTH no-fade
+$MODRINTH notenoughcrashes
+$MODRINTH optigui
+$MODRINTH puzzle
+$CURSE smooth-boot
+$MODRINTH sodium
+$MODRINTH starlight
+$CURSE tooltipfix
+$MODRINTH zoomify
